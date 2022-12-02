e13eb82b08bb589e5cc71f5c8c2dd55aac3bdf48
Your Name <you@example.com>
2022-12-02 07:22:32 +0000

    [IPEHR-172] userGroupGetByID

--- contracts/Access.sol
+++ contracts/Access.sol
@@ -3,39 +3,48 @@ pragma solidity ^0.8.17;
 
 import "./Restrictable.sol";
 
-contract Access is Restrictable {
+contract AccessStore is Restrictable {
     enum AccessLevel { NoAccess, Owner, Admin, Read }
     enum AccessKind { Doc, DocGroup, UserGroup }
 
-    struct Object {
+    struct Access {
         bytes32      idHash;
         bytes        idEncr;    // id encrypted by access key
         bytes        keyEncr;   // access key encrypted by user private key
         AccessLevel  level;
     }
 
-    mapping(bytes32 => Object[]) accessStore;     // accessID => Object[]
+    Access ZeroAccess = Access({
+        idHash: bytes32(0),
+        idEncr: new bytes(0),
+        keyEncr: new bytes(0),
+        level: AccessLevel.NoAccess
+    });
+
+    mapping(bytes32 => Access[]) private accessStore;     // accessID => Access[]
 
     ///
-    function setAccess(bytes32 accessID, Object memory o) internal
+    // Returns 1 on update or 2 on insert
+    function setAccess(bytes32 accessID, Access memory o) internal returns(uint8)
     {
         for(uint i; i < accessStore[accessID].length; i++) {
             if (accessStore[accessID][i].idHash == o.idHash) {
                 accessStore[accessID][i].idEncr = o.idEncr;
                 accessStore[accessID][i].keyEncr = o.keyEncr;
                 accessStore[accessID][i].level = o.level;
-                return;
+                return 1;
             }
         }
 
         accessStore[accessID].push(o);
+        return 2;
     }
 
     ///
-    function getAccessByIdHash(bytes32 accessID, bytes32 objectIdHash) external view returns(Object memory) 
+    function getAccessByIdHash(bytes32 accessID, bytes32 accessIdHash) external view returns(Access memory) 
     {
         for (uint i; i < accessStore[accessID].length; i++){
-            if (accessStore[accessID][i].idHash == objectIdHash) {
+            if (accessStore[accessID][i].idHash == accessIdHash) {
                 return accessStore[accessID][i];
             }
         }
@@ -44,19 +53,18 @@ contract Access is Restrictable {
     }
 
     ///
-    function getUserAccessList(bytes32 accessID) external view returns (Object[] memory) 
+    function getUserAccessList(bytes32 accessID) public view returns (Access[] memory) 
     {
-        require(accessStore[accessID].length > 0, "NFD");
         return accessStore[accessID];
     }
 
     ///
-    function userAccessLevel(bytes32 userID, AccessKind kind, bytes32 idHash) internal view returns (AccessLevel) 
+    function userAccess(bytes32 userID, AccessKind kind, bytes32 idHash) public view returns (Access memory) 
     {
         bytes32 accessID = keccak256(abi.encode(userID, kind));
         for(uint i; i < accessStore[accessID].length; i++){
             if (accessStore[accessID][i].idHash == idHash) {
-                return accessStore[accessID][i].level;
+                return accessStore[accessID][i];
             }
         }
 
@@ -65,11 +73,11 @@ contract Access is Restrictable {
         for (uint i = 0; i < accessStore[accessID].length; i++) {
             for (uint j = 0; j < accessStore[accessID].length; j++) {
                 if (accessStore[accessID][j].idHash == idHash) {
-                    return accessStore[accessID][j].level;
+                    return accessStore[accessID][j];
                 }
             }
         }
 
-        return AccessLevel.NoAccess;
+        return ZeroAccess;
     }
 }
