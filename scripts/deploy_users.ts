8d1cbd0cf6fdd23c2cf6e931afeedc2a89876f73
Your Name <you@example.com>
2022-12-09 06:55:49 +0000

    users separate multicall

---
+++ scripts/deploy_users.ts
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const lib = await ethers.getContractFactory("Attributes");
+  const Lib = await lib.deploy();
+  await Lib.deployed();
+  console.log("Attributes  deployed to:", Lib.address);
+
+  const Users = await ethers.getContractFactory("Users", {
+      libraries: {
+        Attributes: Lib.address
+      }
+  });
+  const users = await Users.deploy("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+  await users.deployed();
+  console.log("Users deployed to:", users.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
